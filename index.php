ac481333bff51a7382c7717d9b856a4ae451c783
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 09:23:02 +0200

    exo2p4 php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <p>
+<?php
+/*Déclaration de la fonction*/
+  function stringDeOuf(){
+    /*Affectation d'une chaine de caractère à la variable string*/
+  $string = 'Ceci est une chaîne de caractère';
+  /*On retourne la variable*/
+  return $string;
+  }
+  /*Affichage du contenu de la fonction*/
+  echo stringDeOuf();
+?>
+    </p>
+  </body>
+</html>
